b2c4eba7f3c3a6a17f2dfba01202d8fd33af459a
Fabrizio Chignoli <chignoli@pobox.com>
2014-03-09 20:50:49 +0100

    Created base commit type, so that event and snapshot are now type aliases

--- EStoria/ValueObjects/CommittedEvent.cs
+++ EStoria/ValueObjects/CommittedEvent.cs
@@ -2,68 +2,7 @@ using System;
 
 namespace EStoria.ValueObjects
 {
-	public class CommittedEvent : IEquatable<CommittedEvent>
-	{
-		public int Serial { get; private set; }
-		public string StreamName { get; private set; }
-		public DateTime TimeStamp { get; private set; }
-		public object Event { get; private set; }
-
-		public CommittedEvent(int serial, string streamName, DateTime timeStamp, object @event)
-		{
-			Serial = serial;
-			StreamName = streamName;
-			TimeStamp = timeStamp;
-			Event = @event;
-		}
-
-		public bool Equals(CommittedEvent other)
-		{
-			if(ReferenceEquals(null, other)) return false;
-			if(ReferenceEquals(this, other)) return true;
-			return Serial == other.Serial;
-		}
-
-		public override int GetHashCode()
-		{
-			return Serial;
-		}
-
-		public static bool operator ==(CommittedEvent left, CommittedEvent right)
-		{
-			return Equals(left, right);
-		}
-
-		public static bool operator !=(CommittedEvent left, CommittedEvent right)
-		{
-			return !Equals(left, right);
-		}
-
-		public static bool operator >(CommittedEvent left, CommittedEvent right)
-		{
-			return left.Serial > right.Serial;
-		}
-
-		public static bool operator <(CommittedEvent left, CommittedEvent right)
-		{
-			return left.Serial < right.Serial;
-		}
-
-		public static bool operator >=(CommittedEvent left, CommittedEvent right)
-		{
-			return left.Serial >= right.Serial;
-		}
-
-		public static bool operator <=(CommittedEvent left, CommittedEvent right)
-		{
-			return left.Serial <= right.Serial;
-		}
-		
-		public override bool Equals(object obj)
-		{
-			if(ReferenceEquals(null, obj)) return false;
-			if(ReferenceEquals(this, obj)) return true;
-			return obj.GetType() == GetType() && Equals((CommittedEvent)obj);
-		}
+	public class CommittedEvent : Commit {
+		public CommittedEvent(int serial, string name, DateTime timeStamp, object data) : base(serial, name, timeStamp, data) {}
 	}
 }
