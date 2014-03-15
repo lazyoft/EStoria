7b3e97951a4c9da7cc4dd9c044c5897127ad04ac
Fabrizio Chignoli <chignoli@pobox.com>
2014-03-15 11:40:48 +0100

    Fixed dangling bracket

--- EStoria/ValueObjects/CommittedEvent.cs
+++ EStoria/ValueObjects/CommittedEvent.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace EStoria.ValueObjects
 {
-	public class CommittedEvent : Commit {
+	public class CommittedEvent : Commit 
+	{
 		public CommittedEvent(int serial, string name, DateTime timeStamp, object data) : base(serial, name, timeStamp, data) {}
 	}
 }
--- EStoria/ValueObjects/CommittedSnapshot.cs
+++ EStoria/ValueObjects/CommittedSnapshot.cs
@@ -2,7 +2,8 @@ using System;
 
 namespace EStoria.ValueObjects
 {
-	public class CommittedSnapshot : Commit {
+	public class CommittedSnapshot : Commit 
+	{
 		public CommittedSnapshot(int serial, string name, DateTime timeStamp, object data) : base(serial, name, timeStamp, data) {}
 	}
 }
