09647aa5a8065d737b4a390f3237a54330c4961f
Fabrizio Chignoli <chignoli@pobox.com>
2014-03-16 08:16:33 +0100

    Created placeholder interfaces for command and domain event

---
+++ EStoria/Interfaces/ICommand.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace EStoria.Interfaces
+{
+	public interface ICommand {}
+}
---
+++ EStoria/Interfaces/IDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace EStoria.Interfaces
+{
+	public interface IDomainEvent
+	{
+		string AggregateId { get; set; }
+	}
+}
