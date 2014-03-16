c9ee5a97f2c1c0c89aa8cfe42c4259574ac38d55
Fabrizio Chignoli <chignoli@pobox.com>
2014-03-16 08:15:47 +0100

    Added command failure value type

---
+++ EStoria/ValueObjects/CommandFailure.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EStoria.Interfaces;
+
+namespace EStoria.ValueObjects
+{
+	public class CommandFailure
+	{
+		public CommandFailure(ICommand command, string reason)
+		{
+			Guard.NotNull(() => command);
+			Guard.NotNullOrWhiteSpace(() => reason);
+
+			Command = command;
+			Reason = reason;
+		}
+
+		public ICommand Command { get; private set; }
+		public string Reason { get; private set; }
+	}
+}
