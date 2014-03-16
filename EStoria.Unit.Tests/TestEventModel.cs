eb2858a8f48afb1ab8125a5c4ea03d302e3134af
Fabrizio Chignoli <chignoli@pobox.com>
2014-03-16 10:34:23 +0100

    EventModel now triggers the subscription after the configuration took place

--- EStoria.Unit.Tests/TestEventModel.cs
+++ EStoria.Unit.Tests/TestEventModel.cs
@@ -8,11 +8,11 @@ namespace EStoria.Unit.Tests
 		public TestEventModel(IObservable<CommittedEvent> events, TestModel modelSnapshot = null, int serial = 0)
 			: base(events, modelSnapshot, serial)
 		{
-			ConfigureEvents
+			HandleEvents(stream => stream
 				.When<string>((model, s) => model.Text += s)
 				.When<int>((model, i) => model.Number += i)
 				.When<DateTime>((model, time) => model.Date = time)
-				.WhenUnknown((model, unknown) => model.Unknown = unknown);
+				.WhenUnknown((model, unknown) => model.Unknown = unknown));
 		}
 	}
 }
